e9b52169a452bce965f3d415cc6b5d6b8d06dfa9
Alferavil88 <alferavil88@gmail.com>
2023-03-21 01:21:38 -0500

    package.json modified

--- knexfile.js
+++ knexfile.js
@@ -4,18 +4,20 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  client: "pg",
-  connection: {
-    host: "dpg-cgcj7lm4dad6fr59ack0-a",
-    database: "toptalentv2_db_6l8v",
-    user: "toptalentv2_db_6l8v_user",
-    password: "GoBSEJj1s1UH7ullevWshstetX4MbrQU",
-  },
-  pool: {
-    min: 2,
-    max: 10,
-  },
-  migrations: {
-    directory: "./db/migrations",
+  development: {
+    client: "pg",
+    connection: {
+      host: "dpg-cgcj7lm4dad6fr59ack0-a",
+      database: "toptalentv2_db_6l8v",
+      user: "toptalentv2_db_6l8v_user",
+      password: "GoBSEJj1s1UH7ullevWshstetX4MbrQU",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
   },
 };
