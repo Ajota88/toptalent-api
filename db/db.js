07445931a47271f6188b9cdc095a19e17a74dad8
Alferavil88 <alferavil88@gmail.com>
2023-03-21 01:39:42 -0500

    fix knexfile

--- db/db.js
+++ db/db.js
@@ -1,18 +1,6 @@
 const knex = require("knex");
 const knexConfig = require("../knexfile");
 
-const db = knex({
-  client: "pg",
-  connection: {
-    host: "dpg-cgcj7lm4dad6fr59ack0-a",
-    database: "toptalentv2_db_6l8v",
-    user: "toptalentv2_db_6l8v_user",
-    password: "GoBSEJj1s1UH7ullevWshstetX4MbrQU",
-  },
-
-  migrations: {
-    directory: "./db/migrations",
-  },
-});
+const db = knex(knexConfig.development);
 
 module.exports = db;
--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,19 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
+require("dotenv").config();
+
 module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: "dpg-cgcj7lm4dad6fr59ack0-a",
-      database: "toptalentv2_db_6l8v",
-      user: "toptalentv2_db_6l8v_user",
-      password: "GoBSEJj1s1UH7ullevWshstetX4MbrQU",
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
     },
-
     migrations: {
       directory: "./db/migrations",
     },
