667bde0b054f9fbab99ce9d133a6bb1b81b3b074
Alferavil88 <alferavil88@gmail.com>
2023-03-21 00:43:52 -0500

    kenxfile added

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  production: {
+    client: "pg",
+    connection: {
+      host: "dpg-cgcj7lm4dad6fr59ack0-a",
+      database: "toptalentv2_db_6l8v",
+      user: "toptalentv2_db_6l8v_user",
+      password: "GoBSEJj1s1UH7ullevWshstetX4MbrQU",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
