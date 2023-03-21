75ac342cbc25e0d4a9a418f9ffa414f8811ca966
Alferavil88 <alferavil88@gmail.com>
2023-03-21 01:02:15 -0500

    fix knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,36 +4,18 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  production: {
-    client: "pg",
-    connection: {
-      host: "dpg-cgcj7lm4dad6fr59ack0-a",
-      database: "toptalentv2_db_6l8v",
-      user: "toptalentv2_db_6l8v_user",
-      password: "GoBSEJj1s1UH7ullevWshstetX4MbrQU",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "./db/migrations",
-    },
+  client: "pg",
+  connection: {
+    host: "dpg-cgcj7lm4dad6fr59ack0-a",
+    database: "toptalentv2_db_6l8v",
+    user: "toptalentv2_db_6l8v_user",
+    password: "GoBSEJj1s1UH7ullevWshstetX4MbrQU",
   },
-  development: {
-    client: "pg",
-    connection: {
-      host: "dpg-cgcj7lm4dad6fr59ack0-a",
-      database: "toptalentv2_db_6l8v",
-      user: "toptalentv2_db_6l8v_user",
-      password: "GoBSEJj1s1UH7ullevWshstetX4MbrQU",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "./db/migrations",
-    },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    directory: "./db/migrations",
   },
 };
