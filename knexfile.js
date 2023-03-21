8f642464ef6455d35f622bcd62f3ed5da09d2259
Alferavil88 <alferavil88@gmail.com>
2023-03-21 00:51:02 -0500

    knexfile fix

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,23 @@ module.exports = {
       max: 10,
     },
     migrations: {
-      tableName: "knex_migrations",
+      directory: "./db/migrations",
+    },
+  },
+  development: {
+    client: "pg",
+    connection: {
+      host: "dpg-cgcj7lm4dad6fr59ack0-a",
+      database: "toptalentv2_db_6l8v",
+      user: "toptalentv2_db_6l8v_user",
+      password: "GoBSEJj1s1UH7ullevWshstetX4MbrQU",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./db/migrations",
     },
   },
 };
